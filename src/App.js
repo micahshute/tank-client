deec73f34ffc5842c66fe62d14287f6b28c40410
Micah Shute <micah.shute@gmail.com>
2019-02-10 01:10:57 -0600

    create ajax requests for CORS testing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import RequestManager from './requests/test'
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      data: ""
+    }
+  }
+
+  async componentDidMount(){
+    const rm = new RequestManager()
+    const data = await rm.users()
+    this.setState({
+      data
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {this.state.data}
           </p>
           <a
             className="App-link"
---
+++ src/requests/test.js
@@ -0,0 +1,72 @@
+export default class RequestTests{
+
+    constructor(){
+        this.baseURL = "http://192.168.1.4:3000/api/"
+    }
+
+    async users(){
+        const url = this.baseURL + "users"
+        return await this.dispatchGetRequest(url)
+    }
+
+    async user(id){
+        const url = this.baseURL + `users/${id}`
+    }
+
+    async signup(username, password){
+        const url = this.baseURL + `signup`
+        const form = this.createLoginForm(username, password)
+        const formData = new FormData(form)
+        try{
+            const data = await fetch(url, {
+                method: "POST",
+                headers: this.formHeaders()
+            })
+        }catch(e){
+            return `${e}`
+        }
+    }
+
+    async dispatchGetRequest(url){
+        try{
+            const data = await fetch(url, {
+                method: "GET",
+                headers: this.getHeaders()
+            }).then(res => res.json())
+            return data
+        }catch(e){
+            return `${e}`
+        }
+    }
+
+    formHeaders(){
+        return {
+            "Accept": "application/json", 
+        }
+    }
+
+    getHeaders(){
+        return {
+            "Accept": 'application/json'
+        }
+    }
+
+    createLoginForm(username, password){
+        const form = document.createElement('form')
+        const usernameInput = document.createElement('input')
+        usernameInput.name = "username"
+        usernameInput.value = username
+        usernameInput.type = "text"
+        const passwordInput = document.createElement("input")
+        passwordInput.name = "password"
+        passwordInput.value = password
+        passwordInput.type = "password"
+        const submitInput = document.createElement("input")
+        submitInput.type = "submit"
+        form.appendChild(usernameInput)
+        form.appendChild(passwordInput)
+        form.appendChild(submitInput)
+        return form
+    }
+
+}
